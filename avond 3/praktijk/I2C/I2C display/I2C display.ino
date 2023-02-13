ee33b7a35c7a22ea678974fa5748d5081d87e7da
Riekusb <Riekus.bennink@gmail.com>
2023-02-13 14:41:17 +0100

    Layout voorbeeld toegevoegd

--- avond 3/praktijk/I2C/I2C display/I2C display.ino
+++ avond 3/praktijk/I2C/I2C display/I2C display.ino
@@ -6,26 +6,40 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 4);  // set the LCD address to 0x27 for a 16 chars and 4line display
 
-void setMyCursor(int row,int col){ // error in library 
-   if( row <1){
-     lcd.setCursor(col, row);
-   }else{
-     lcd.setCursor(col-4, row);
-   }
+void set1604Cursor(int row, int col) {  // error in library
+  if (row < 1) {
+    lcd.setCursor(col, row);
+  } else {
+    lcd.setCursor(col - 4, row);
+  }
 }
 
 void setup() {
   lcd.init();  // initialize the lcd
   // Print a message to the LCD.
   lcd.backlight();
-  setMyCursor(0, 0);
+  set1604Cursor(0, 0);
   lcd.print("Regel 1");
-  setMyCursor(1,0);
+  set1604Cursor(1, 0);
   lcd.print("Regel 2");
-  setMyCursor(2, 0);
+  set1604Cursor(2, 0);
   lcd.print("Regel 3");
-  setMyCursor(3, 0);
+  set1604Cursor(3, 0);
   lcd.print("Regel 4");
+  delay(5000);
+  lcd.clear();
+  
+  set1604Cursor(0, 0);
+  lcd.print("Ma-10-02 10:56");
+  set1604Cursor(1,0);
+  lcd.print("PH  6,80");
+  set1604Cursor(2, 0);
+  lcd.print("Temp  25,7 C");
+ set1604Cursor(3, 0);
+  lcd.print("Licht 80%");
+
+
+
 }
 
 
